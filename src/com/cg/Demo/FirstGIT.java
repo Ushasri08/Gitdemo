a652cff5ff3975e55226dc26ed6203e2bfdcf865
SRI <pilli-usha.sri@capgemini.com>
2024-03-06 13:23:36 +0530

    FirstCommit

---
+++ src/com/cg/Demo/FirstGIT.java
@@ -0,0 +1,11 @@
+package com.cg.Demo;
+
+public class FirstGIT {
+
+	public static void main(String[] args) {
+		System.out.println("Hey!! Welcome to GIT");
+		System.out.println("Gi branch created");
+
+	}
+
+}
---
+++ src/com/cg/Demo/SecondGIT.java
@@ -0,0 +1,10 @@
+package com.cg.Demo;
+
+public class SecondGIT {
+
+	public static void main(String[] args) {
+		System.out.println("Hey!!second GIT");
+
+	}
+
+}
